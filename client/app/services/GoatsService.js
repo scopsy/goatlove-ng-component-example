f8248f40546d86a319c2ef73365a5ddfefd12b0d
Evgeniy Aksyonov <evgeniy.aksyonov@cloudmade.com>
2017-10-09 17:56:20 +0300

    Updated angular-ui-router version. Installed Bootstrap/ Installed babel-register package. Updated page heading.

--- client/app/services/GoatsService.js
+++ client/app/services/GoatsService.js
@@ -7,7 +7,7 @@ function GoatsService() {
             name: "Goat 1",
             age: 23,
             about: "I'm a demo goat, and this is my bio",
-            photo: "http://images.northrup.org/picture/xl/animals/billy-goat-profile1.jpg"
+            photo: "https://i.ytimg.com/vi/nlYlNF30bVg/hqdefault.jpg"
         }
     ];
 
